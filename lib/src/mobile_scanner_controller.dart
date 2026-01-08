fea5c2a9c5ea4c8e6bdb7b53ee2aae6204e66b47
longnguyen-fastboy <long_nguyen@fastboy.net>
2026-01-08 07:57:34 +0700

    add state

--- lib/src/mobile_scanner_controller.dart
+++ lib/src/mobile_scanner_controller.dart
@@ -475,7 +475,8 @@ class MobileScannerController extends ValueNotifier<MobileScannerState> {
           isStarting: false,
           isRunning: true,
           size: viewAttributes.size,
-          deviceOrientation: viewAttributes.initialDeviceOrientation,
+          deviceOrientation: DeviceOrientation.portraitUp,
+          // deviceOrientation: viewAttributes.initialDeviceOrientation,
           // Provide the current torch state.
           // Updates are provided by the `torchStateStream`.
           torchState: viewAttributes.currentTorchMode,
